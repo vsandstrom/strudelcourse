7b8b4e14e6938d15a26ec36e639c86d5ab67c7bd
Viktor Sandström <sandstrom.viktor@gmail.com>
2024-03-14 09:43:44 +0100

    layout

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,9 +12,18 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    // prerender: {
-    //   entries: ['*', '/', '/note', '/sound', '/stack', '/ping_pong', '/samples']
-    // },
+    prerender: {
+      entries: [
+        '*', 
+        '/', 
+        '/note', 
+        '/sound', 
+        '/stack', 
+        '/ping_pong', 
+        '/samples',
+        '/tutorial'
+      ]
+    },
     paths: {
       base: process.env.NODE_ENV === "production" ? "/strudelcourse" : "",
     }
