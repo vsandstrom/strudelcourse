38991f1cd762db65e83e89390c52402535ba0580
Viktor Sandström <sandstrom.viktor@gmail.com>
2024-03-14 10:01:26 +0100

    strict false

--- svelte.config.js
+++ svelte.config.js
@@ -11,19 +11,19 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
-    // prerender: {
-    //   entries: [
-    //     '*', 
-    //     '/', 
-    //     '/note', 
-    //     '/sound', 
-    //     '/stack', 
-    //     '/ping_pong', 
-    //     '/samples',
-    //     '/tutorial'
-    //   ]
-    // },
+    adapter: adapter({strict: false}),
+    prerender: {
+      entries: [
+        '*', 
+        '/', 
+        '/note', 
+        '/sound', 
+        '/stack', 
+        '/ping_pong', 
+        '/samples',
+        '/tutorial'
+      ]
+    },
     paths: {
       base: process.env.NODE_ENV === "production" ? "/strudelcourse" : "",
     }
