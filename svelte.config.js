5704273d2031378f79b7fa35f5acf8497b344c52
Viktor Sandström <sandstrom.viktor@gmail.com>
2024-03-14 09:49:43 +0100

    based

--- svelte.config.js
+++ svelte.config.js
@@ -12,18 +12,18 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    prerender: {
-      entries: [
-        '*', 
-        '/', 
-        '/note', 
-        '/sound', 
-        '/stack', 
-        '/ping_pong', 
-        '/samples',
-        '/tutorial'
-      ]
-    },
+    // prerender: {
+    //   entries: [
+    //     '*', 
+    //     '/', 
+    //     '/note', 
+    //     '/sound', 
+    //     '/stack', 
+    //     '/ping_pong', 
+    //     '/samples',
+    //     '/tutorial'
+    //   ]
+    // },
     paths: {
       base: process.env.NODE_ENV === "production" ? "/strudelcourse" : "",
     }
