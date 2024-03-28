b9d2f48540db1398d3b144a29d312f021b81e49c
Viktor Sandström <sandstrom.viktor@gmail.com>
2024-03-28 11:41:52 +0100

    added forgotten svelte file

---
+++ src/lib/ShortCut.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import {base} from '$app/paths';
+
+</script>
+
+
+<div id="shortcut">
+  <p>Shortcuts:</p> 
+  <div id="sc">
+    <a href="{base}/sound#drums">Drum Kit</a>
+    <a href="{base}/note#piano">Piano Keys</a>
+  </div>
+</div>
+
+<style>
+  #sc {
+    display: flex;
+    justify-content: right;
+    flex-direction: column;
+  }
+
+  #shortcut {
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    justify-content:flex-end;
+    padding-bottom: 0.3em;
+    margin-top: -0.4em;
+    font-family: monospace;
+    font-size: 0.8em;
+  }
+
+  #shortcut  a {
+    padding-bottom: 0.5em;
+  }
+
+#shortcut p {
+  margin-top: -0.2em;
+  margin-right: 1.4em;
+}
+
+</style>
